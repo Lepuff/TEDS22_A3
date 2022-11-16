f1bd7413fa53bf9d2612b0bc610758232261111f
Oskar Persson <oskar11.persson@gmail.com>
2022-11-16 13:43:23 +0100

    rdflib  and template for the graph

--- subscriber.py
+++ subscriber.py
@@ -3,14 +3,47 @@ import time
 import numpy as np
 import datetime
 from rdflib.namespace import RDF, RDFS, XSD, SOSA, TIME
-
+from rdflib import Namespace
 messages = []
 messagescount = 0
 
 
 
+# define the rdf graph
 
 
+
+
+""""
+@prefix rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>.
+@prefix xsd:  <http://www.w3.org/2001/XMLSchema#> .
+@prefix sosa: <http://www.w3.org/ns/sosa/> .
+@prefix time: <http://www.w3.org/2006/time#>.
+@prefix qudt-1-1: <http://qudt.org/1.1/schema/qudt#> .
+@prefix qudt-unit-1-1: <http://qudt.org/1.1/vocab/unit#> .
+@prefix cdt: <http://w3id.org/lindt/custom_datatypes#> .
+@base <http://example.org/data/> .
+
+# The barometric readings from a Bosch Sensortec BMP282 sensor in an Apple IPhone 7
+# observed on June 6 2017 using only the SOSA core for modelling.
+
+<earthAtmosphere> rdf:type sosa:FeatureOfInterest ;
+  rdfs:label "Atmosphere of Earth"@en .
+
+
+# An iPhone 7 as the Platform that hosts several sensors,
+# among others the Bosch Sensortec BMP282 atmospheric pressure sensor.
+
+<iphone7/35-207306-844818-0> a sosa:Platform ;
+  rdfs:label "IPhone 7 - IMEI 35-207306-844818-0"@en ;
+  rdfs:comment "IPhone 7 - IMEI 35-207306-844818-0 - John Doe"@en ;
+  sosa:hosts <sensor/35-207306-844818-0/BMP282> .
+
+<sensor/35-207306-844818-0/BMP282> rdf:type sosa:Sensor ;
+  rdfs:label "Bosch Sensortec BMP282"@en ;
+  sosa:observes <sensor/35-207306-844818-0/BMP282/atmosphericPressure> .
+"""
 
 print("creating new instance")
 client = mqtt.Client("P2")     # create new instance (the ID, in this case "P1", must be unique)
